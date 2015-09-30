138226de4e164329299091f8a6080669cf87cf1b
Storycoder <joseph.margolis@gmail.com>
2015-09-30 11:30:49 -0400

    configure Guardfile and test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'mocha/mini_test'
+
+require 'minitest/reporters'
+Minitest::Reporters.use!(Minitest.Reporters::ProgressReporter.new)
+
+require 'capybara/rails'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
